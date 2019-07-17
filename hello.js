74dc74a157eda04ce514ceac6fa567f7d7dcd797
Shahid <shahid.sarker@gmail.com>
2019-07-17 19:12:51 -0400

    fix?

--- hello.js
+++ hello.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 1337;
 http
   .createServer(function(req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello Travis!\n");
+    res.send("Hello Travis!\n");
   })
   .listen(1337, "127.0.0.1");
 
