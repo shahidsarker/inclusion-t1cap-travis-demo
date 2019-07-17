e7ad3adcc9eaefa64529ea05ae477e4cf96bd983
Shahid <shahid.sarker@gmail.com>
2019-07-17 19:05:32 -0400

    test

--- hello.js
+++ hello.js
@@ -1,9 +1,12 @@
-var http = require('http');
+var http = require("http");
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Travis!\n');
-}).listen(1337, '127.0.0.1');
+const port = process.env.PORT || 1337;
 
-console.log('Server running at http://127.0.0.1:1337/');
+http
+  .createServer(function(req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello Travis!\n");
+  })
+  .listen(1337, "127.0.0.1");
 
+console.log("Server running at http://127.0.0.1:1337/");
