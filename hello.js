cb9ecc19287fa84824eba733cf05f6dc38f0c912
Chris Choyce <christopher.choyce@gmail.com>
2019-07-17 19:21:52 -0400

    Refactored IP address var

--- hello.js
+++ hello.js
@@ -1,11 +1,12 @@
 var http = require('http');
 
 const port = process.env.PORT || 1337;
+const ipAddress = '0.0.0.0';
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis!\n');
-}).listen(port, '0.0.0.0');
+}).listen(port, ipAddress);
 
-console.log(`Server running at http://127.0.0.1:${port}/`);
+console.log(`Server running at http://${ipAddress}:${port}/`);
 
