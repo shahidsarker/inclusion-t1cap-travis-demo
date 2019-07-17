a2aeabbbea6419460f6ac08d4d8e37f5ce43ad7f
Chris Choyce <christopher.choyce@gmail.com>
2019-07-17 19:11:45 -0400

    Change ip of server

--- hello.js
+++ hello.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 1337;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis!\n');
-}).listen(port, '127.0.0.1');
+}).listen(port, '0.0.0.0');
 
 console.log(`Server running at http://127.0.0.1:${port}/`);
 
